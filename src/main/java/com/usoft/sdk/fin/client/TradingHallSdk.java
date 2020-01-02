a4b7cd59c2b57461794519d9497d1f817ae66db2
ljh <linjh@usoftchina.com>
2020-01-02 10:11:26 +0800

    1、获取融资方信息列表 2、根据验订单ID通过b2b获取订单详情 3、根据验收单主表ID通过b2b获取验收单详情 4、通过enuu获取融资方信息接口

---
+++ src/main/java/com/usoft/sdk/fin/client/TradingHallSdk.java
@@ -0,0 +1,74 @@
+package com.usoft.sdk.fin.client;
+
+import com.usoft.fin.external.open.api.protobuf.*;
+import com.usoft.sdk.fin.utils.HttpUtil;
+import com.usoft.sdk.fin.utils.ProtoBufUtil;
+
+import java.util.Map;
+
+
+/**
+ * @author: linjh
+ * @date: 2019-12-30 17:00
+ **/
+public class TradingHallSdk extends BaseSdk {
+    public TradingHallSdk(String baseUrl, String secretId, String secretKey) {
+        super(baseUrl, secretId, secretKey);
+    }
+
+    public TradingHallSdk(String baseUrl, String secretId, String secretKey, int timeout) {
+        super(baseUrl, secretId, secretKey, timeout);
+    }
+
+    /**
+     * [获取融资方列表]
+     * @author: linjh
+     * @date: 2019/12/30
+     **/
+    public GetFinanceSidesResp getFinanceSides(GetFinanceSidesReq.Builder req) throws Exception {
+        String url = baseUrl + "/api/open/factoring/financeSides";
+        Map<String, String> params = genSignToMap(req);
+        String respJson = HttpUtil.doGet(url, params, timeout);
+        GetFinanceSidesResp.Builder resp = ProtoBufUtil.toProtoBuf(GetFinanceSidesResp.newBuilder(), respJson);
+        return resp.build();
+    }
+
+    /**
+     * [根据验订单ID通过b2b获取订单详情]
+     * @author: linjh
+     * @date: 2019/12/31
+     **/
+    public GetOrderByOrderIdResp getOrderByOrderId(GetOrderByOrderIdReq.Builder req) throws Exception {
+        String url = baseUrl + "/api/open/factoring/getOrderByOrderId";
+        Map<String, String> params = genSignToMap(req);
+        String respJson = HttpUtil.doGet(url, params, timeout);
+        GetOrderByOrderIdResp.Builder resp = ProtoBufUtil.toProtoBuf(GetOrderByOrderIdResp.newBuilder(), respJson);
+        return resp.build();
+    }
+
+    /**
+     * [根据验收单主表ID通过b2b获取验收单详情]
+     * @author: linjh
+     * @date: 2019/12/31
+     **/
+    public GetAcceptOrderByAcceptIdResp GetAcceptOrderByAcceptId(GetAcceptOrderByAcceptIdReq.Builder req) throws Exception {
+        String url = baseUrl + "/api/open/factoring/getAcceptOrderByAcceptId";
+        Map<String, String> params = genSignToMap(req);
+        String respJson = HttpUtil.doGet(url, params, timeout);
+        GetAcceptOrderByAcceptIdResp.Builder resp = ProtoBufUtil.toProtoBuf(GetAcceptOrderByAcceptIdResp.newBuilder(), respJson);
+        return resp.build();
+    }
+
+    /**
+     * [通过enuu获取融资方信息]
+     * @author: linjh
+     * @date: 2019/12/31
+     **/
+    public GetBaseInfoResp getBaseInfo(GetBaseInfoReq.Builder req) throws Exception {
+        String url = baseUrl + "/api/open/factoring/baseInfo";
+        Map<String, String> params = genSignToMap(req);
+        String respJson = HttpUtil.doGet(url, params, timeout);
+        GetBaseInfoResp.Builder resp = ProtoBufUtil.toProtoBuf(GetBaseInfoResp.newBuilder(), respJson);
+        return resp.build();
+    }
+}
---
+++ src/test/java/com/usoft/sdk/fin/TradingHallSdkTest.java
@@ -0,0 +1,54 @@
+package com.usoft.sdk.fin;
+
+import com.usoft.fin.external.open.api.protobuf.*;
+import com.usoft.sdk.fin.client.TradingHallSdk;
+import com.usoft.sdk.fin.utils.ProtoBufUtil;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author: linjh
+ * @date: 2019-12-30 17:37
+ **/
+public class TradingHallSdkTest {
+    /**
+     * 测试地址
+     */
+    private TradingHallSdk tradingHallSdk = new TradingHallSdk("https://finrest.uuzcc.cn", "10042875", "c49f7be6a861461ab951e55030055a5c");
+
+    @Test
+    public void getFinanceSides() throws Exception {
+        GetFinanceSidesReq.Builder req = GetFinanceSidesReq.newBuilder();
+        req.setPageNumber(1);
+        req.setPageSize(10);
+        GetFinanceSidesResp resp = tradingHallSdk.getFinanceSides(req);
+        System.out.println(ProtoBufUtil.toJSON(resp));
+    }
+
+    @Test
+    public void getOrderByOrderId() throws Exception {
+        GetOrderByOrderIdReq.Builder req = GetOrderByOrderIdReq.newBuilder();
+        req.setEnuu(10041961);
+        req.setOrderId(14125390);
+        GetOrderByOrderIdResp resp = tradingHallSdk.getOrderByOrderId(req);
+        System.out.println(ProtoBufUtil.toJSON(resp));
+    }
+
+    @Test
+    public void GetAcceptOrderByAcceptId() throws Exception {
+        GetAcceptOrderByAcceptIdReq.Builder req = GetAcceptOrderByAcceptIdReq.newBuilder();
+        req.setEnuu(10000666);` `
+        req.setAcceptIdPrimary(1856822);
+        GetAcceptOrderByAcceptIdResp resp = tradingHallSdk.GetAcceptOrderByAcceptId(req);
+        System.out.println(ProtoBufUtil.toJSON(resp));
+    }
+
+    @Test
+    public void getBaseInfo() throws Exception {
+        GetBaseInfoReq.Builder req = GetBaseInfoReq.newBuilder();
+        req.setEnuu(10005285);
+        req.setCsEnuu(10050856);
+        req.setUu(10005285);
+        GetBaseInfoResp resp = tradingHallSdk.getBaseInfo(req);
+        System.out.println(ProtoBufUtil.toJSON(resp));
+    }
+}
